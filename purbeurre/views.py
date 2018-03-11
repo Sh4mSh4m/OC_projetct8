bec5028601ebed12a84022a5b486a51d2172803f
hieu@mb12 <hieu2d@gmail.com>
2018-03-11 18:00:44 +0100

    index uploaded

--- purbeurre/views.py
+++ purbeurre/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
+from .models import Product
 
 def index(request):
-	return HttpResponse("Salut c'est Bobby")
+    latest_product = Product.objects.order_by('-name_product')[:3]
+    context = {
+        'latest_product': latest_product,
+    }
+    return render(request, 'purbeurre/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
