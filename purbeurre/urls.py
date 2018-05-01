69e4fa3422b362be48c525abfd321a0aa287c9ec
Sh4M <hieu2d@gmail.com>
2018-05-01 17:01:50 +0200

    index search and substitute proposal is effective

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -9,13 +9,9 @@ urlpatterns = [
     # ex: /purbeurre/
     path('', views.IndexView.as_view(), name='index'),
     # ex: /purbeurre/5/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:product_id>/', views.detail, name='detail'),
     # ex: /purbeurre/search/banane%20de%20mer
-    path('search/<str:product_searched>', views.search, name='search'),
-    # ex: /polls/5/results/
-    path('substitute/<str:product_searched>', views.substitute, name='substitute'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('search', views.search, name='search'),
+    # ex: /purbeurre/substitute/name
+    path('substitute/<int:product_id>', views.substitute, name='substitute'),
 ]
--- purbeurre/views.py
+++ purbeurre/views.py
@@ -20,23 +20,13 @@ def detail(request, product_id):
     product = get_object_or_404(Products, pk=product_id)
     return render(request, 'purbeurre/detail.html', {'product':product})
 
-def search(request, product_searched):
+def search(request):
+    product_searched= request.GET.get('query')
     products = Products.objects.filter(name__istartswith=product_searched)[:9]
     return render(request, 'purbeurre/search.html', {'products':products, 'product_searched': product_searched})
 
-def substitute(request, product_searched):
-    product = Products.objects.filter(name__istartswith=product_searched)[0]
+def substitute(request, product_id):
+    product = get_object_or_404(Products, pk=product_id)
     substitutes = Products.objects.exclude(brands=product.brands).filter(main_category=product.main_category).filter(nutrition_grade__gte=product.nutrition_grade).order_by('-nutrition_grade')[:6]
     return render(request, 'purbeurre/substitute.html', {'product':product, 'substitutes': substitutes})
-#    return HttpResponse("You're looking at question %s." % product.name)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-#    substitutes = Products.objects.exclude(brands=product.brands).filter(main_category=product.main_category).filter(nutrition_grade__gte=product.nutrition_grade).order_by('-nutrition_grade')[:6]
 
