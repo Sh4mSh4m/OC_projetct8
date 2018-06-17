5eaee503549ed2d59f958bafc752ddf24b850ae4
hieu@mb12 <hieu2d@gmail.com>
2018-06-17 18:34:24 +0200

    uninstall Sentry

--- OC_project8/settings/__init__.py
+++ OC_project8/settings/__init__.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['209.97.141.179', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'raven.contrib.django.raven_compat',
     'purbeurre.apps.PurbeurreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
